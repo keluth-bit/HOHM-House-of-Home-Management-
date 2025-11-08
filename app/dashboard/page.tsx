7aac60e3b7a625add5cebe847292b5e9510cf3c7
Lucy Lu <lucylu@Lucys-MBP.fios-router.home>
2025-11-07 19:40:11 -0500

    Cleaned up Prisma and removed auth

--- app/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function DashboardPage() {
-  return (
-    <main className="min-h-screen flex items-center justify-center">
-      <h1 className="text-2xl font-bold">Welcome to Your HOHM Dashboard!</h1>
-    </main>
-  )
-}
